d4187591ca96b3aa24410abd8531ac5485928429
MadakaYogeshwar <iosdevyogesh@gmail.com>
2020-10-06 16:40:26 +0530

    added UUID

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const Joi=require('joi');
-const express=require('express');
-const app=express();
+//CRUD(CREATE-READ-UPDATE-DELETE) EXAMPLE FOR GENERES
+const uuid = require('uuid-random');
+const Joi = require('joi');
+const express = require('express');
+const app = express();
 app.use(express.json());
-let generes=[
-    {id:1,name: 'Action'},
-    {id:2,name: 'Thriller'},
-    {id:3,name: 'Romance'}
+let generes = [
+    {id: uuid(), name: 'Action'},
+    {id: uuid(), name: 'Thriller'},
+    {id: uuid(), name: 'Romance'}
 ];
 app.get('/api/generes', (req, res) => {
     res.send(generes);
@@ -13,7 +15,7 @@ app.get('/api/generes', (req, res) => {
 });
 //path variable(:id)
 app.get('/api/generes/:id', (req, res) => {
-    let id=parseInt(req.params.id);
+    let id = req.params.id;
     const genre = generes.find(c => c.id === id);
     if (!genre)
         return res.status(404).send(`The genre with the ID ${id} was not found.`);
@@ -25,21 +27,21 @@ app.get('/api/generes/:id', (req, res) => {
 });
 
 app.post('/api/generes', (req, res) => {
-    const { error } = validateGenre(req.body);
+    const {error} = validateGenre(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
     const genre = {
-        id: generes.length + 1,
+        id: uuid(),
         name: req.body.name
     };
     generes.push(genre);
     res.send(genre);
 });
 app.put('/api/generes/:id', (req, res) => {
-    const genre = generes.find(c => c.id === parseInt(req.params.id));
+    const genre = generes.find(c => c.id === req.params.id);
     if (!genre) return res.status(404).send('The genre with the given ID was not found.');
 
-    const { error } = validateGenre(req.body);
+    const {error} = validateGenre(req.body);
     if (error) return res.status(400).send(error.details[0].message);
     console.log(genre.name);
     genre.name = req.body.name;
@@ -48,7 +50,7 @@ app.put('/api/generes/:id', (req, res) => {
 });
 
 app.delete('/api/generes/:id', (req, res) => {
-    const genre = generes.find(c => c.id === parseInt(req.params.id));
+    const genre = generes.find(c => c.id === req.params.id);
     if (!genre) {
         return res.status(404).send('The genre with the given ID was not found.');
     }
@@ -59,9 +61,10 @@ app.delete('/api/generes/:id', (req, res) => {
 
 });
 
-let port=process.env.PORT || 3000;
-app.listen(port, function(){
-    console.log('server started listening on port number',port);
+
+let port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log('server started listening on port number', port);
 });
 
 function validateGenre(genre) {
