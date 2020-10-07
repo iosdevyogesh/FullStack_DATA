412009394a41af4863c91a865eb8c6da5518abd5
MadakaYogeshwar <iosdevyogesh@gmail.com>
2020-10-07 10:33:49 +0530

    Served static content

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(express.static('public'))
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+app.listen(3000);
