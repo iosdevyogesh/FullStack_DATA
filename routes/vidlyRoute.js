8a5be125921a5c4b34de27c180b230914f57f7e8
MadakaYogeshwar <iosdevyogesh@gmail.com>
2020-10-12 10:01:40 +0530

    Added remove

--- routes/vidlyRoute.js
+++ routes/vidlyRoute.js
@@ -34,6 +34,30 @@ router.put('/:id', async (req, res) => {
 
 });
 
+router.put('/:id', async (req, res) => {
+    const {error} = validateGenre(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+    //updates all records
+    //const genre = await Genre.findByIdAndUpdate(req.params.id, {name: req.body.name},{new:true});
+    //update One record
+    const genre = await Genre.findOneAndUpdate(req.params.id, {name: req.body.name},{new:true});
+    /*const genre = await Genre.update({ _id: req.params.id }, {
+        $set: { name: req.body.name}
+    });*/
+    if ((!genre)) {
+        return res.status(404).send('The genre with the given ID was not found.');
+    }
+    res.send(genre);
+});
+
+router.delete('/:id', async (req, res) => {
+    const genre = await Genre.findByIdAndRemove(req.params.id);
+    if (!genre) return res.status(404).send('The genre with the given ID was not found.');
+    res.send(genre);
+});
+
 function validateGenre(genre) {
     const schema = {
         name: Joi.string().min(5).required()
